52fdf7def9f5869e14b6997a2a1d8f12baa056fe
ip1259 <ip1259@gmail.com>
2022-12-07 22:34:12 +0800

    JavaScript修改，新增動態側邊攔功能

--- scripts/page_picture.js
+++ scripts/page_picture.js
@@ -1,8 +1,19 @@
-import _pictures from "./picInfos.js";
+
 function changePicture(pic_id){
     document.getElementById("_picDisplay").src = _pictures[pic_id].src
     document.getElementById("_picTitle").innerHTML = _pictures[pic_id].title;
     document.getElementById("_picAuthor").innerHTML = _pictures[pic_id].author;
     document.getElementById("_picDscrp").innerHTML = _pictures[pic_id].description;
+}
+
+function sidebarList(_pictures){
+    let pictures = Array.from(_pictures)
+    let output = ""
+    let i = 0
+    pictures.forEach(element => {
+        output += '<li><a href="javascript:changePicture('+ i +')" id="lstPic_'+i+'" onclick="">'+element.title+'</a></li>\n'
+        i++
+    });
+    return output
 }
 
--- scripts/picInfos.js
+++ scripts/picInfos.js
@@ -26,5 +26,18 @@ const _picMountain = {
     月亮則像探照燈,意圖將身上的蛀蟲找出,在絕望中掙扎著。\
     </p>"
 };
-const _pictures = [_picCastle,_picMountain];
-export default _pictures;
+const _picItalyFog = {
+    src:"images/italy_fog.jpg",
+    title:"Lontaly",
+    author:"Albert Itelas Wombo",
+    description:
+    "<p>創作於2002年,取材於西亞地區的某山區,\
+    作者將植被去除,山區只剩下灰白的外表,顯示著對於過度開發的憂慮,\
+    搭配上碩大的月亮與雲霧裊裊透露著孤獨,以此構建出了近未來的末世風格的畫作。\
+    </p>\
+    <p>畫的是山卻名為石,訴說著作者認為沒了樹林與植被,山不再是山不過是冰冷的石頭,\
+    了了無幾的植物象徵剩餘的生機,頑強且拼命的生存著,雲霧像自然的防護衣,妄圖阻擋蛀蟲侵入,\
+    月亮則像探照燈,意圖將身上的蛀蟲找出,在絕望中掙扎著。\
+    </p>"
+};
+const _pictures = [_picCastle,_picMountain,_picItalyFog];

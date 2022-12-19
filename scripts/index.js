205ec1aad166f7966a4908fc0a9ee084a9a98333
ip1259 <ip1259@gmail.com>
2022-12-19 17:21:29 +0800

    頁首圖片輪播，添加導覽列效果

---
+++ scripts/index.js
@@ -0,0 +1,19 @@
+﻿(function () { //使變數為區域變數避免其他來源檔的干擾
+    const nav = document.querySelector('nav')
+    const navHeight = nav.offsetHeight
+
+    // // Handle animation end
+    // function handleAnimationEnd(element, animations) {
+    //     element.classList.remove(...animations)
+    // }
+
+    // Handle navbar animation
+    function animateNav() {
+        if (window.pageYOffset > navHeight) { nav.classList.add('blue', 'darken-4', 'shadow') }
+        else { nav.classList.remove('blue', 'darken-4', 'shadow') }
+    }
+
+    window.addEventListener('scroll', () => {
+        animateNav()
+    })
+})()
---
+++ scripts/materialize-init.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    // // Init Scrollspy
+    // $('.scrollspy').scrollSpy();
+    // Init slider
+    $('.slider').slider({
+        indicators: false,
+        interval: 5000
+    });
+});
